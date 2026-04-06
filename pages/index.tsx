536cdc3d0c20907152a36e9fb83e7e3bcbf46184
Aleksei <mail-kochetkov@yandex.ru>
2026-04-06 14:14:31 +0300

    Add an input field

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,17 @@
-import React from 'react'; const Home: React.FC = () => { return <h1>Welcome to the Homepage</h1>; }; export default Home;
+import React, { useState } from 'react';
+
+const Field = () => {
+    const [subject, setSubject] = useState('');
+
+    return (
+        <div className="flex flex-col">
+            <label className="mb-2">Тема заявки </label>
+            <input type="text" value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+                         className="border rounded-md p-2"/>
+            <br/>Это пример надписи на странице.
+        </div>
+    );
+};
+
+export default Field;
